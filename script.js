95aa2801530de72b6a753951b82a70038bd6de4d
jillwilliams <codeGeek.03.14@gmail.com>
2020-08-25 13:53:22 -0700

    Final Draft

---
+++ script.js
@@ -0,0 +1,71 @@
+const myProblem = () => {
+    var answer = document.getElementById("a1").value;
+    if (answer == "25x^2 + 6") {
+        return "Good Job!";
+    } else 
+        return "Try Again!"
+};
+const myCalc = () => {
+    document.getElementById("demo").innerHTML = myProblem();
+}
+
+const myProblem2 = () => {
+        return "25x^2 + 6";
+};
+const myCalc2 = () => {
+    document.getElementById("demo2").innerHTML = myProblem2();
+}
+
+const myProblem3 = () => {
+    var answer = document.getElementById("a2").value;
+    if (answer == "3x + 18") {
+        return "Good Job!";
+    } else 
+        return "Try Again!"
+};
+const myCalc3 = () => {
+    document.getElementById("demo3").innerHTML = myProblem3();
+}
+
+const myProblem4 = () => {
+        return "3x + 18";
+};
+const myCalc4 = () => {
+    document.getElementById("demo4").innerHTML = myProblem4();
+}
+
+const myProblem5 = () => {
+    var answer = document.getElementById("a3").value;
+    if (answer == "2x + 5") {
+        return "Good Job!";
+    } else 
+        return "Try Again!"
+};
+const myCalc5 = () => {
+    document.getElementById("demo5").innerHTML = myProblem5();
+}
+
+const myProblem6 = () => {
+        return "2x + 5";
+};
+const myCalc6 = () => {
+    document.getElementById("demo6").innerHTML = myProblem6();
+}
+
+const myProblem7 = () => {
+    var answer = document.getElementById("a4").value;
+    if (answer == "10x - 3") {
+        return "Good Job!";
+    } else 
+        return "Try Again!"
+};
+const myCalc7 = () => {
+    document.getElementById("demo7").innerHTML = myProblem7();
+}
+
+const myProblem8 = () => {
+        return "10x - 3";
+};
+const myCalc8 = () => {
+    document.getElementById("demo8").innerHTML = myProblem8();
+}
